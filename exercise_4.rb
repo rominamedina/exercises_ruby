df4989018ec2a9832e3e9cb3b3bc880ba82544d3
Romina Medina <rmedinacpn@gmail.com>
2022-02-17 21:00:05 -0300

    exercise_4
    method that when entering the age shows a sequence of ascending numbers from 1 to the entered age

--- exercise_4.rb
+++ exercise_4.rb
@@ -1,2 +1,14 @@
 #Escribir un programa que pregunte al usuario su edad y muestre por pantalla todos los años que ha cumplido 
 #(desde 1 hasta su edad).
+
+def show_age
+
+  puts 'How old are you?'
+
+  age = gets.chomp.to_i
+
+  1.upto(age) { |i| puts i }
+
+end
+
+show_age
